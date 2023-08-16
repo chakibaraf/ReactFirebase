920ef78d35c8e9332e2acca382ed67a60f665246
unknown <araf.chakib.m@gmail.com>
2023-08-16 16:38:09 +0200

    features private rout

--- auth-react/src/App.js
+++ auth-react/src/App.js
@@ -2,6 +2,8 @@ import {Routes,Route} from "react-router-dom"
 import Home from "./pages/Home";
 import Navbar from "./component/Navbar";
 import SignUpModal from "./component/SignUpModal";
+import Private from "./pages/Private";
+import PrivateHome from "./pages/PrivateHome/PrivateHome";
 
 function App() {
   return (
@@ -10,6 +12,14 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/private" element={<Private/>}>
+        <Route path="/private/private-home" element={<PrivateHome/>}/>
+        
+        </Route>
+     
+  
+
+       
     </Routes>
      
     </>
---
+++ auth-react/src/pages/PrivateHome/PrivateHome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Ron from "./Ron.gif"
+
+export default function PrivateHome() {
+  return (
+    <div className='container p-5'>
+      <h1 className=' diplay-3 text-light mb-4' >
+        Page Home
+      </h1>
+      <img src={Ron} alt="Ron peur" srcset="" />
+    </div>
+  )
+}
